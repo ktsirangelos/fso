afecad53df40f1c56eb3fe4fa1ecf866cb3955f1
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-06-05 11:21:39 +0200

    refactor anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,60 +1,64 @@
 import { useState } from "react";
 
-const anecdotes = [
-  "If it hurts, do it more often.",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-  "The only way to go fast, is to go well.",
-];
-
-const initialVotes = new Array(anecdotes.length).fill(0);
-
-const DisplayAnecdote = (props) => {
-  return <p>{anecdotes[props.selected]}</p>;
+const DisplayAnecdote = ({ anecdote }) => {
+  return <p>{anecdote}</p>;
 };
 
-const DisplayVotes = (props) => {
-  return <p>has {props.votes} votes</p>;
+const DisplayVotes = ({ votes }) => {
+  return <p>has {votes} votes</p>;
 };
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const AnecdoteOfDay = (props) => {
+const AnecdoteOfDay = ({
+  anecdotes,
+  selected,
+  votes,
+  handleVoteClick,
+  handleNextClick,
+}) => {
   return (
     <div>
       <h1>Anecdote of the day</h1>
-      <DisplayAnecdote selected={props.selected} />
-      <DisplayVotes votes={props.votes[props.selected]} />
+      <DisplayAnecdote anecdote={anecdotes[selected]} />
+      <DisplayVotes votes={votes[selected]} />
       <Button
         handleClick={() => {
-          props.handleVoteClick(props.selected);
+          handleVoteClick(selected);
         }}
         text="vote"
       />
-      <Button handleClick={props.handleNextClick} text="next anecdote" />
+      <Button handleClick={handleNextClick} text="next anecdote" />
     </div>
   );
 };
 
-const AnecdoteMostVotes = (props) => {
+const AnecdoteMostVotes = ({ anecdotes, selected, votes }) => {
   return (
     <div>
       <h1>Anecdote with most votes</h1>
-      <DisplayAnecdote selected={props.selected} />
-      <DisplayVotes votes={props.votes[props.selected]} />
+      <DisplayAnecdote anecdote={anecdotes[selected]} />
+      <DisplayVotes votes={votes[selected]} />
     </div>
   );
 };
 
 const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
   const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(initialVotes);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
   const [highest, setHighest] = useState(0);
 
   const handleNextClick = () => {
@@ -73,18 +77,23 @@ const App = () => {
       }
     });
     setVotes(updatedVotes);
-    setHighest(+updatedVotes.indexOf(Math.max(...updatedVotes)));
+    setHighest(updatedVotes.indexOf(Math.max(...updatedVotes)));
   };
 
   return (
     <div>
       <AnecdoteOfDay
+        anecdotes={anecdotes}
         selected={selected}
         votes={votes}
         handleNextClick={handleNextClick}
         handleVoteClick={handleVoteClick}
       />
-      <AnecdoteMostVotes selected={highest} votes={votes} />
+      <AnecdoteMostVotes
+        selected={highest}
+        votes={votes}
+        anecdotes={anecdotes}
+      />
     </div>
   );
 };
