b478c3aa90ec7621579fd56bedb86b62f772e94f
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-08-02 13:08:37 +0200

    index & app

--- part4/index.js
+++ part4/index.js
@@ -1,24 +1,8 @@
-const cors = require('cors')
-const mongoose = require('mongoose')
-const express = require('express')
-const middleware = require('./utils/middleware')
-
-const app = express()
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
+
