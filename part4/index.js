5ba4eafc81ab4367aeee5096d5d107d1c1d43fe7
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-08-02 13:02:20 +0200

    controllers/blogs

--- part4/index.js
+++ part4/index.js
@@ -7,30 +7,14 @@ const app = express()
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const Blog = require('./models/blog')
-
+const blogsRouter = require('./controllers/blogs')
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
