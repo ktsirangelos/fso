0bdced2b4caf3d92c40a7b8dff56970ca2ee173d
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-08-03 12:46:51 +0200

    4.3 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
