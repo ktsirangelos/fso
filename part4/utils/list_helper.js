f3f8df726d97429f9725b20738b936cdfee32b9c
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-08-03 14:30:44 +0200

    4.4 & 4.5 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,10 +8,32 @@ const totalLikes = (blogs) => {
   } else if (blogs.length === 1) {
     return blogs[0].likes
   } else {
-    return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+    return blogs.reduce((likesAccumulator, blog) => likesAccumulator + blog.likes, 0)
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 'none'
+  } else if (blogs.length === 1) {
+    return [{
+      _id: blogs[0]._id,
+      title: blogs[0].title,
+      author: blogs[0].author,
+      url: blogs[0].url,
+      likes: blogs[0].likes,
+      __v: blogs[0].__v,
+    }]
+  } else {
+    const maxLikesBlog = blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
+    return {
+      title: maxLikesBlog.title,
+      author: maxLikesBlog.author,
+      likes: maxLikesBlog.likes
+    }
   }
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
