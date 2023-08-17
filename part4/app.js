29879c9471e7430fb9c60637632f64dff92f4754
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-08-17 16:31:03 +0200

    4.18 done

--- part4/app.js
+++ part4/app.js
@@ -6,8 +6,9 @@ const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
@@ -28,6 +29,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 
 
