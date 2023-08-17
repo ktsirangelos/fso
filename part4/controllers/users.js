7a45e7cd8f7be4bbef082150572254275cf82a29
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-08-17 16:04:08 +0200

    4.17 done

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 // ATTENTION! Errors are handled by 'express-async-errors'
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
   response.json(users)
 })
 
